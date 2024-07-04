7ca9485662255beafec2bee5ae24ce2ac7affb67
Harshad Panmand <panmandharshaddev@gmail.com>
2024-07-04 09:57:02 +0530

    Leetcode 104

---
+++ src/leetcode/Problem104.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+public class Problem104 {
+    // Definition for a binary tree node.
+    static public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.left.left.left = new TreeNode(7);
+
+        System.out.println(maxDepth(root));
+    }
+
+    static public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
+    }
+}
