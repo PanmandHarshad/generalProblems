d9aec69a60d69a86a02603cd2d500d2de95fc832
Harshad Panmand <panmandharshaddev@gmail.com>
2024-12-16 19:15:34 +0530

    Leetcode 1071

--- src/leetcode/Problem1071.java
+++ src/leetcode/Problem1071.java
@@ -3,9 +3,17 @@ package leetcode;
 public class Problem1071 {
 
     public static void main(String[] args) {
+        System.out.println(gcdOfStrings("ABABAB", "ABAB"));
         System.out.println(gcdOfStrings("abcabc", "abc"));
         System.out.println(gcdOfStrings("ababab", "ab"));
         System.out.println(gcdOfStrings("leet", "code"));
+        System.out.println(gcdOfStrings("abcabcabca", "abc"));
+
+        System.out.println(gcdOfStrings1("ABABAB", "ABAB"));
+        System.out.println(gcdOfStrings1("abcabc", "abc"));
+        System.out.println(gcdOfStrings1("ababab", "ab"));
+        System.out.println(gcdOfStrings1("leet", "code"));
+        System.out.println(gcdOfStrings1("abcabcabca", "abc"));
     }
 
     static public String gcdOfStrings(String str1, String str2) {
@@ -25,5 +33,19 @@ public class Problem1071 {
             a = temp;
         }
         return a;
+    }
+
+    static public String gcdOfStrings1(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        if (str1.length() == str2.length()) {
+            return str1;
+        } else if (str1.length() > str2.length()) {
+            return gcdOfStrings1(str1.substring(str2.length()), str2);
+        } else {
+            return gcdOfStrings1(str1, str2.substring(str1.length()));
+        }
     }
 }
