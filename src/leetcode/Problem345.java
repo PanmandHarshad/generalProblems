19f885f281b9dbdd1c65a0dcbc1f989fd3794e78
Harshad Panmand <panmandharshaddev@gmail.com>
2025-01-05 19:33:28 +0530

    Leetcode 345 Improved the existing solution and added another solution

--- src/leetcode/Problem345.java
+++ src/leetcode/Problem345.java
@@ -9,6 +9,7 @@ public class Problem345 {
         System.out.println(reverseVowels("leetcode"));
     }
 
+    // Most efficient solution
     static public String reverseVowels(String s) {
         int start = 0;
         int end = s.length() - 1;
@@ -17,20 +18,77 @@ public class Problem345 {
         while (start < end) {
             char startingChar = arr[start];
             char endingChar = arr[end];
-            if (startingChar == 'a' || startingChar == 'e' || startingChar == 'i' || startingChar == 'o' || startingChar == 'u') {
-                if (endingChar == 'a' || endingChar == 'e' || endingChar == 'i' || endingChar == 'o' || endingChar == 'u') {
+            if (startingChar == 'a' || startingChar == 'e' || startingChar == 'i' || startingChar == 'o' || startingChar == 'u' ||
+                    startingChar == 'A' || startingChar == 'E' || startingChar == 'I' || startingChar == 'O' || startingChar == 'U') {
+                if (endingChar == 'a' || endingChar == 'e' || endingChar == 'i' || endingChar == 'o' || endingChar == 'u' ||
+                        endingChar == 'A' || endingChar == 'E' || endingChar == 'I' || endingChar == 'O' || endingChar == 'U') {
                     arr[start] = endingChar;
                     arr[end] = startingChar;
                     start++;
-                    end--;
-                }else {
-                    end--;
                 }
+                end--;
             } else {
                 start++;
             }
         }
 
         return String.valueOf(arr);
+    }
+
+    static public String reverseVowels1(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        char[] charArray = s.toCharArray();
+        while (start < end) {
+            boolean startCharVowel = isVowel(s.charAt(start));
+            boolean endCharVowel = isVowel(s.charAt(end));
+            if (startCharVowel && endCharVowel) {
+                charArray[start] = s.charAt(end);
+                charArray[end] = s.charAt(start);
+                start++;
+                end--;
+            } else if (!startCharVowel) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return String.valueOf(charArray);
+    }
+
+    static public boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+
+    static public String reverseVowels2(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        String vowels = "aeiouAEIOU";
+        char[] charArray = s.toCharArray();
+        while (left < right) {
+
+            while (left < right && vowels.indexOf(s.charAt(left)) == -1) {
+                left++;
+            }
+
+            while (left < right && vowels.indexOf(s.charAt(right)) == -1) {
+                right--;
+            }
+
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return String.valueOf(charArray);
     }
 }
