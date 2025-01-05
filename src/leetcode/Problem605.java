386eeee5d925d953ee9927645b91f39ed1629736
Harshad Panmand <panmandharshaddev@gmail.com>
2025-01-05 18:31:03 +0530

    Leetcode 605 another solution

--- src/leetcode/Problem605.java
+++ src/leetcode/Problem605.java
@@ -3,10 +3,17 @@ package leetcode;
 public class Problem605 {
 
     public static void main(String[] args) {
-//        System.out.println(canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1));
-//        System.out.println(canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2));
+        System.out.println(canPlaceFlowers(new int[]{0, 0, 1, 0, 1}, 1));
+        System.out.println(canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1));
+        System.out.println(canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2));
         System.out.println(canPlaceFlowers(new int[]{1, 0, 0, 0, 0, 1}, 2));
         System.out.println(canPlaceFlowers(new int[]{1, 0, 0, 0, 0, 0, 1}, 2));
+
+        System.out.println(canPlaceFlowers1(new int[]{0, 0, 1, 0, 1}, 1));
+        System.out.println(canPlaceFlowers1(new int[]{1, 0, 0, 0, 1}, 1));
+        System.out.println(canPlaceFlowers1(new int[]{1, 0, 0, 0, 1}, 2));
+        System.out.println(canPlaceFlowers1(new int[]{1, 0, 0, 0, 0, 1}, 2));
+        System.out.println(canPlaceFlowers1(new int[]{1, 0, 0, 0, 0, 0, 1}, 2));
     }
 
     static public boolean canPlaceFlowers(int[] flowerbed, int n) {
@@ -23,5 +30,24 @@ public class Problem605 {
         }
 
         return n == 0;
+    }
+
+    static public boolean canPlaceFlowers1(int[] flowerbed, int n) {
+        int length = flowerbed.length;
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (flowerbed[i] == 0 &&
+                    (i == 0 || flowerbed[i - 1] == 0) &&
+                    (i == length - 1 || flowerbed[i + 1] == 0)) {
+
+                flowerbed[i] = 1;
+                count++;
+
+                if (count >= n) {
+                    return true;
+                }
+            }
+        }
+        return count >= n;
     }
 }
