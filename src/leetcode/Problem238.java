1d7a014f81c25f5063b39d061090c577b78ce715
Harshad Panmand <panmandharshaddev@gmail.com>
2025-01-05 21:54:19 +0530

    Leetcode 238 Added efficient solution

--- src/leetcode/Problem238.java
+++ src/leetcode/Problem238.java
@@ -2,12 +2,12 @@ package leetcode;
 
 public class Problem238 {
     public static void main(String[] args) {
-        int[] ints = productExceptSelf(new int[]{1, 2, 3, 4});
+        int[] ints = productExceptSelf1(new int[]{1, 2, 3, 4});
         for (int num : ints) {
             System.out.print(num + ", ");
         }
         System.out.println();
-        int[] ints1 = productExceptSelf(new int[]{-1, 1, 0, -3, 3});
+        int[] ints1 = productExceptSelf1(new int[]{-1, 1, 0, -3, 3});
         for (int num : ints1) {
             System.out.print(num + ", ");
         }
@@ -17,7 +17,8 @@ public class Problem238 {
         int n = nums.length;
         int[] result = new int[n];
 
-        if (n == 0) return result;
+        if (n == 0)
+            return result;
 
         if (n == 1) {
             result[0] = 1;
@@ -44,4 +45,21 @@ public class Problem238 {
         return result;
     }
 
+    // Most efficient solution
+    static public int[] productExceptSelf1(int[] nums) {
+        int length = nums.length;
+        int[] result = new int[length];
+
+        result[0] = 1;
+        for (int i = 1; i < length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int suffix = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            result[i] = result[i] * suffix;
+            suffix = suffix * nums[i];
+        }
+        return result;
+    }
 }
