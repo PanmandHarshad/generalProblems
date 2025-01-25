408fc04668b51ad226f35ef0aadd96ef5b51e3a4
Harshad Panmand <panmandharshaddev@gmail.com>
2025-01-25 19:39:20 +0530

    Leetcode 643: Improved the performance

--- src/leetcode/Problem643.java
+++ src/leetcode/Problem643.java
@@ -2,7 +2,7 @@ package leetcode;
 
 public class Problem643 {
     public static void main(String[] args) {
-//        System.out.println(findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4));
+        System.out.println(findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4));
         System.out.println(findMaxAverage(new int[]{0, 1, 1, 3, 3}, 4));
     }
 
@@ -12,16 +12,15 @@ public class Problem643 {
             sum += nums[i];
         }
 
-        // Initialize maxSum with the sum of the first k elements
         double maxSum = sum;
 
-        // Slide the window from start to end of the array
         for (int i = k; i < nums.length; i++) {
-            sum += nums[i] - nums[i - k];  // Add the next element and remove the element left behind
-            maxSum = Math.max(maxSum, sum);  // Update maxSum if the current sum is greater
+            sum += nums[i] - nums[i - k];
+            if (sum > maxSum)
+                maxSum = sum;
         }
 
-        // Return the maximum average
         return maxSum / k;
     }
+
 }
