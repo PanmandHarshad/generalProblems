f4eacefa409e94c99af60aa5ab9808c79f129b46
Harshad Panmand <panmandharshaddev@gmail.com>
2025-02-22 21:03:41 +0530

    Leetcode 2390: Added most efficient solution

--- src/leetcode/Problem2390.java
+++ src/leetcode/Problem2390.java
@@ -1,28 +1,42 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Stack;
-
 public class Problem2390 {
 
     public static void main(String[] args) {
         System.out.println(removeStars("leet**cod*e"));
         System.out.println(removeStars("erase*****"));
+
+        System.out.println(removeStars1("leet**cod*e"));
+        System.out.println(removeStars1("erase*****"));
     }
 
+    // Most efficient solution
     static public String removeStars(String s) {
+        char[] arr = s.toCharArray();
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '*') {
+                j--;
+            } else {
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+        return new String(arr, 0, j);
+    }
+
+    static public String removeStars1(String s) {
         StringBuilder sb = new StringBuilder();
-        int stars = 0;
-        for (int i = s.length() - 1; i >= 0; --i) {
-            char ch = s.charAt(i);
+
+        for (char ch : s.toCharArray()) {
             if (ch == '*') {
-                ++stars;
-            } else if (stars > 0) {
-                --stars;
+                if (!sb.isEmpty()) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
             } else {
                 sb.append(ch);
             }
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
