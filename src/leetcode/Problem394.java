7755bdd8a0c11107e2e475cdb867bee932c7a1a5
Harshad Panmand <panmandharshaddev@gmail.com>
2025-03-02 15:48:04 +0530

    Leetcode 394: Added most efficient solution

--- src/leetcode/Problem394.java
+++ src/leetcode/Problem394.java
@@ -4,8 +4,8 @@ import java.util.Stack;
 
 public class Problem394 {
     public static void main(String[] args) {
-        System.out.println(decodeString("3[a]2[bc]")); // Output: aaabcbc
-        System.out.println(decodeString("3[a2[c]]")); // Output: accaccacc
+        System.out.println(decodeString1("3[a]2[bc]")); // Output: aaabcbc
+        System.out.println(decodeString1("3[a2[c]]")); // Output: accaccacc
     }
 
     public static String decodeString(String s) {
@@ -34,6 +34,38 @@ public class Problem394 {
             }
         }
 
+        return currentString.toString();
+    }
+
+    // Most efficient solution
+    public static String decodeString1(String s) {
+        Stack<String> stack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int currentNo = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                currentNo = currentNo * 10 + (ch - '0');
+            } else if (ch == '[') {
+                stack.push(currentString.toString());
+                stack.push(String.valueOf(currentNo));
+                currentNo = 0;
+                currentString = new StringBuilder();
+            } else if (ch == ']') {
+                int repeatCount = Integer.parseInt(stack.pop());
+                String prevString = stack.pop();
+
+                StringBuilder repeated = new StringBuilder();
+                repeated.append(prevString);
+
+                while (repeatCount-- > 0) {
+                    repeated.append(currentString);
+                }
+                currentString = repeated;
+            } else {
+                currentString.append(ch);
+            }
+        }
         return currentString.toString();
     }
 }
