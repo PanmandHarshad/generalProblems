97c4d569ac9495be6a83e2a7312ab27523ef9ad9
Harshad Panmand <panmandharshaddev@gmail.com>
2025-03-16 21:51:54 +0530

    Leetcode 328: Added examples in the main method and updated the if condition

--- src/leetcode/Problem328.java
+++ src/leetcode/Problem328.java
@@ -19,25 +19,43 @@ public class Problem328 {
     }
 
     public static void main(String[] args) {
-        Problem328 solution = new Problem328();
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);
 
-        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        head = solution.oddEvenList(head);
+        ListNode head2 = new ListNode(2);
+        head2.next = new ListNode(1);
+        head2.next.next = new ListNode(3);
+        head2.next.next.next = new ListNode(5);
+        head2.next.next.next.next = new ListNode(6);
+        head2.next.next.next.next.next = new ListNode(4);
+        head2.next.next.next.next.next.next = new ListNode(7);
 
-        ListNode current = head;
-        print(current);
-        // Output: 1 3 5 2 4
+        System.out.println("Before rearranging:");
+        printList(head1);
+        head1 = oddEvenList(head1);
+        System.out.println("After rearranging:");
+        printList(head1);
+
+        System.out.println("Before rearranging:");
+        printList(head2);
+        head2 = oddEvenList(head2);
+        System.out.println("After rearranging:");
+        printList(head2);
     }
 
-    private static void print(ListNode current) {
+    private static void printList(ListNode current) {
         while (current != null) {
             System.out.print(current.val + " ");
             current = current.next;
         }
+        System.out.println();
     }
 
-    public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null) {
+    static public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
             return head;
         }
 
@@ -45,17 +63,14 @@ public class Problem328 {
         ListNode even = head.next;
         ListNode evenHead = even;
 
-        // Iterate through the list, separating odd and even nodes
         while (even != null && even.next != null) {
-            odd.next = even.next; // Link odd nodes together
-            odd = odd.next; // Move odd pointer forward
-            even.next = odd.next; // Link even nodes together
-            even = even.next; // Move even pointer forward
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
         }
 
         odd.next = evenHead;
         return head;
     }
-
-
 }
