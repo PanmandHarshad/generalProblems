e872d7e0a203de035a2a54f74450eee75e1818de
Harshad Panmand <panmandharshaddev@gmail.com>
2025-03-22 20:27:39 +0530

    Leetcode 206: Added main and print method

--- src/leetcode/Problem206.java
+++ src/leetcode/Problem206.java
@@ -19,6 +19,28 @@ public class Problem206 {
         }
     }
 
+    public static void main(String[] args) {
+        // Creating a test linked list: 1 -> 2 -> 3 -> 4 -> 5 -> null
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        System.out.println("Reversed List:");
+        printList(reverseList(head));
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
 
     static public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
